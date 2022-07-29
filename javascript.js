38d0011548719d7ffb40e38dd031c5d76b859055
tkdudgns95 <tkdudgns95@naver.com>
2022-07-29 20:59:56 +0900

    first commit

---
+++ javascript.js
@@ -0,0 +1,30 @@
+ /* const name = prompt("이름을 입력하세요.");
+  alert("환영합니다, " + name + "님"); */
+ 
+ /* const name = prompt("예약일을 입력해주세요.", "2020-10"); (제목, 디폴트값(미리 입력 되있는 것)) */
+ 
+
+  /*const isAdult = confirm("당신은 성인 입니까?");
+    console.log(isAdult) */
+
+    /* alert: 메세지를 보여줌.
+      prompt: 메세지를 보여주고, 어떤 값을 입력할 수 있는 필드를 제공한다. 취소를 누르면 False 반환
+      confirm: 사용자에게 확인을 받기위한 용도 (확인은 True 취소는 False 반환) */
+
+        
+     /*   const mathScore = prompt("수학 몇점?");
+         const engScore = prompt("영어 몇점?");
+         const result = (mathScore + engScore) / 2;
+
+         console.log(result) */
+
+      /* String() 문자형으로 변환
+
+         Number() 숫자형으로 변환 -> 넘버형 안의 문자는 모두 NaN(False)
+         Number(null) 은 0이됨, Number(undefined) 는 NaN이 된다.
+
+         Number(0) 은 False, Number('0') 은 true
+         Number("") 은 False Number(' ') (공백)은 true
+
+         Boolean() 불린형으로 변환 -> 0,""(문자열), undefined, NaN 모두 False 반환.  */
+ 
