96f6260e720a523f531879e997d100dfdbae8f39
tkdudgns95 <tkdudgns95@naver.com>
2022-08-16 23:23:51 +0900

    자바스크립트 변수

---
+++ javajoong.js
@@ -0,0 +1,23 @@
+// var는 한 번 선언된 변수를 다시 선언할 수 있다.
+/*
+var name = 'Mike';
+console.log(name); // Mike
+
+var name = 'Jane';
+console.log(name); // Jane */
+
+// let은 두번 선언될 수 없다.
+/*
+let name = 'Mike';
+console.log(name); // Mike
+
+let name = 'Jane'; // error!
+console.log(name); */
+
+/*
+var name; // 호이스팅: 스코프 내부 어디서든 변수 선언은 최상위에 선언된 것 처럼 행동.
+
+console.log(name);
+
+name = 'Mike'; */
+
